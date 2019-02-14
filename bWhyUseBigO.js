94c566eb2fda94ce9218b7eb837ece0be0cdbef3
April Copes <antdevelopment1@gmail.com>
2019-02-13 20:33:14 -0500

    made corrections to files

--- bWhyUseBigO.js
+++ bWhyUseBigO.js
@@ -3,31 +3,45 @@
 // ==========
 // Why use Big O? Can we test how performant code is by using timing
 
-// We can use a built in method called performance.now() and do some basic subtraction to calculate how long it takes to run a file when 
-// no arguments are passed to a function vs when we pass for example 10,000 as an argument. We can take the increased time from passing 
-// a value to a function and subtract the value when no arguments were passed to the function to calculate the difference for time elapsed. 
-// For example…
+// We can use a built in method called performance.now() (in the broswer) and do some basic subtraction to calculate how long it takes 
+// to run this function when no arguments are passed to a function vs when we pass for example 10,000 as an argument. We can take the
+// increased time from passing  a value to a function and subtract the value when no arguments were passed to the function to calculate 
+// the difference for time elapsed. For example…
 
-// Based on the value passed to n as argument calculate the sum and return the total    
-function addSum(num) {
-	let total = 0;
-	for (let i = 0; i < num.length; i++) {
-		total += num[i];
+// Based on the value passed to n as argument accumulate the sum into a variable and return the total?
+// ===========================
+// Slower Linear O(N) Solution  
+// ===========================
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+      total += i;
     }
     return total;
 }
+  
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
 
-time1 = performance.now();
-	addSum(100000)
-	Time2 = performance.now()
-	Time2 - Time1 / 1000  //(to convert into seconds) will equal the time elapsed
-	// We do the same operations for this function
-	function addSum(num) {
-		return n * (num + 1) / 2;
+// =============================
+// Faster Constant O(1) Solution  
+// =============================
+function addUpTo(n) {
+    return n * (n + 1) / 2;
 }
+  
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
 
-time1 = performance.now();
-	addSum(100000)
-	Time2 = performance.now()
-	Time2 - Time1 / 1000 //(to convert into seconds) will equal the time elapsed
-// The time elapsed for the second function seems much faster and performant than the first.
+// =================================================
+// The problem with using built in timing functions
+// ================================================
+// The problem with this approach is different computers will record different times. For this reason it isn’t reliable. 
+// Though using this approach does tell us that the second solution is more performant, the output from doing it this way creates 
+// a system that is inconsistently measurable. Also, even using the same computer to perform these calculations will vary and not 
+// the same even when running the same function with the same inputs and outputs. And for fast algorithms, tracking the speed may 
+// not be precise enough to really evaluate what’s happening under the hood or at least from a big picture perspective.
---
+++ cWhatIsBetter.js
@@ -0,0 +1,16 @@
+// ==========
+// Question:
+// ==========
+
+// 1. So how do we talk about and compare code in general terms without having to run performant operations to try to evaluate the efficiency 
+// of our code?
+
+// 2. What does better mean when refering to Big O Notation?
+// Does it mean faster?
+// Does it mean it takes up less memory?
+// Does it mean it’s more readable?
+// Is it shorter in length as a solution?
+
+// For time complexity we care more about how fast the code runs or how long it takes for code to execute and for space complexity we 
+// care about memory or the number of operations used or created in an algorithm
+
