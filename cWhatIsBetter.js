c5701a29f42bff4731b6fadba8d49df1551c0dbd
April Copes <antdevelopment1@gmail.com>
2019-02-13 20:46:32 -0500

    what is better

--- cWhatIsBetter.js
+++ cWhatIsBetter.js
@@ -14,3 +14,12 @@
 // For time complexity we care more about how fast the code runs or how long it takes for code to execute and for space complexity we 
 // care about memory or the number of operations used or created in an algorithm
 
+// 3. What is a good way of calculating our code if not timing functions?
+// Rather than using timing functions which can vary from computer to computer we can count the number of simple operations is a given 
+// algorithm as that remains constant regardless of what computer we are using. The time it takes for a task to execute will be directly 
+// relative to the number of operations in an algorithm so it doesn’t matter what computer we are using or if it’s acting differently on a 
+// given day. The time may vary or be different but again the time elapsed will based on the number of simple operations inside a given 
+// algorithm.
+
+
+
