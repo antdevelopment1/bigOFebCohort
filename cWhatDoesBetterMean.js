034e94970eaeab19cc9df4f8d36a772c2848402b
April Copes <antdevelopment1@gmail.com>
2019-02-13 21:03:30 -0500

    what does better mean file name change

---
+++ cWhatDoesBetterMean.js
@@ -0,0 +1,25 @@
+// ==========
+// Question:
+// ==========
+
+// 1. So how do we talk about and compare code in general terms without having to run performant operations to try to evaluate the efficiency 
+// of our code?
+
+// 2. What does better mean when refering to Big O Notation?
+// Does it mean faster?
+// Does it mean it takes up less memory?
+// Does it mean it’s more readable?
+// Is it shorter in length as a solution?
+
+// For time complexity we care more about how fast the code runs or how long it takes for code to execute and for space complexity we 
+// care about memory or the number of operations used or created in an algorithm
+
+// 3. What is a good way of calculating our code if not timing functions?
+// Rather than using timing functions which can vary from computer to computer we can count the number of simple operations is a given 
+// algorithm as that remains constant regardless of what computer we are using. The time it takes for a task to execute will be directly 
+// relative to the number of operations in an algorithm so it doesn’t matter what computer we are using or if it’s acting differently on a 
+// given day. The time may vary or be different but again the time elapsed will based on the number of simple operations inside a given 
+// algorithm.
+
+
+
