660a961ad77f7045483355ac2ae40fda353b8595
April Copes <antdevelopment1@gmail.com>
2019-02-14 22:00:31 -0500

    real world application

---
+++ bRealwordcases.js
@@ -0,0 +1,5 @@
+// Real World Cases of Big O Notation and Who uses it
+// From the searches that come up on google it's not described as something that is used at companies. Not to say that no companies use big O
+// It's more to say that it's an important concept to be able to converse about the efficiency of our code and as data scales in size 
+// we need consider how we write our code so that in can be performant in the real world. It's an important thing to understand as a part
+// of computer science fundamentals and it can be commonly asled about interview situations.
--- jIntroIntoProblemSolving.js
+++ jIntroIntoProblemSolving.js
@@ -1,3 +1,92 @@
 // ============================================
 // Some things to consider when problem solving
 // ============================================
+
+// Techniques to solving problems
+// Write a function that takes two numbers and returns their sum
+// =================================
+// Understanding the Problem Step 1:
+// =================================
+// Can I restate the problem into my won words
+
+// What are the input that go into the problem
+
+// What are the outputs that should come from the solution of the problem
+
+// Can the outputs be determined from the inputs? In other words do I have enough infornmation to solve the problem for example did the user pass
+// the needed arguments
+
+// How should I label the important pieces of data that are part of the problem
+
+// ==========================
+// Concrete Examples Step 2:
+// ==========================
+
+// Write a function that takes a string and returns counts of each character in the string
+
+// Start with simple examples
+
+// Progress to more complex examples
+
+// Explore examples with empty inputs
+
+// Explore examples with invalid inputs
+
+
+
+// =====================
+// Break It Down Step 3:
+// =====================
+
+// Write a function that takes a string and returns counts of each character in the string
+
+// function charCount(str) {
+
+    // Make an object to return to the end
+
+    // Lower case input
+
+    // Evalute each letter inside of string
+        // If the character is a number/letter and is a key in object, add one to count
+        // If the character is a number/letter and is not a key in the object, add it to the object and set its value to 1
+        // If the character is something else  (space, period, ...etc) don't do anything
+
+    // Return an object with each letter and the correct count as the output
+// }
+
+
+function charCount(str) {
+
+    // Make an object to return to the end
+    var object = {};
+
+    // Evalute each letter inside of string
+    for (var char of str) {
+        // We call the function alphaNumeric and pass it the current letter we are looping through
+        if (isAlphaNumeric(char)) {
+            // We lowercase the letter
+            char = char.toLowerCase();
+            // If the value is in our object we will increment it or just set it to one if its not in our object
+            object[char] = ++object[char] || 1;
+        }
+    }
+
+    console.log(object);
+
+}
+
+// This function just checks to see if the letter we pass it is lowercase, uppercase, or a number. If so return true and continue above
+function isAlphaNumeric(char) {
+
+    var code = char.charCodeAt(0);
+
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code >96 && code < 123)) {
+            return false;
+        } else {
+            return true;
+        }
+}
+
+charCount('hello how are you there I\'m doing really well. How about you?');
