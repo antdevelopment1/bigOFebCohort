7e567f30dda320a57c3f9693113fb001427b22f9
C Barton <43180845+CB987@users.noreply.github.com>
2019-02-15 07:45:38 -0500

    Update eCommonRunTimeComplexities.js
    I think having the definition right by the notation helps clarify

--- eCommonRunTimeComplexities.js
+++ eCommonRunTimeComplexities.js
@@ -2,7 +2,12 @@
 // Question:
 // ==========
 // What are the common runtime complexities we will talk about today?
-// O(N), O(1), 0(N*N) or O(N^2)
+
+// O(N), (f(n) = n) Linear- As n scales as the input the runtime scales as well
+//O(1), (f(n) = 1) Constant - As n grows it doesn’t have much impact because the runtime is always constant
+//0(N*N) or O(N^2), (f(n) = n2) Quadratic - As n grows the runtimes squares related to the square of n
+
+
 // Other common runtime complexities. There are more but these are the most common ones.
 // Logarithmic: O(log n)
 // Lineararithmic: O(n log n)
@@ -12,11 +17,6 @@
 
 // Good picure of all runtime complexities
 'https://adrianmejia.com/blog/2018/04/05/most-popular-algorithms-time-complexity-every-programmer-should-know-free-online-tutorial-course/#All-running-complexities-graphs'
-
-// What does each time complexity mean?
-// (f(n) = n) Linear- As n scales as the input the runtime scales as well
-// (f(n) = n2) Quadratic - As n grows the runtimes squares related to the square of n
-// (f(n) = 1) Constant - As n grows it doesn’t have much impact because the runtime is always constant
 
 // How can we visualize runtime complexity with real example?
 'https://rithmschool.github.io/function-timer-demo/'
