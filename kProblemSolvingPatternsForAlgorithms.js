0774d5a96b7323f2d16bc441dcd6eb71906da3ab
April Copes <antdevelopment1@gmail.com>
2019-02-14 22:07:05 -0500

    freq pattern

--- kProblemSolvingPatternsForAlgorithms.js
+++ kProblemSolvingPatternsForAlgorithms.js
@@ -1,6 +1,10 @@
 // ===========================
 // Problem Solving Patterns
 // ===========================
-// Frequency - 
+// Frequency - This pattern uses objects or sets to collect values or frequency of values. It's a bit more involved than using nested loops
+// But it is more efficient
 // Multiple Pointers - 
 // Sliding Positional - 
+// Dynamic Programming - 
+// Greedy Algorithms  - 
+// Backtracking - 
